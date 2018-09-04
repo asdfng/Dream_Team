6cefbfb7cf9d1f6213aebed74460fc8da3d7483c
npratt8 <40443831+npratt8@users.noreply.github.com>
2018-09-04 14:29:16 -0500

    updated to pull info from server for 3 minutes (once a second)

--- Import_JSON_coordinates.py
+++ Import_JSON_coordinates.py
@@ -1,8 +1,14 @@
-import json, urllib.request
+i = 0
+while i < 180:
+    import json, urllib.request, time
 
-with urllib.request.urlopen('http://172.16.0.1:8001/FieldData/GetData') as response:
-    source = response.read()
+    with urllib.request.urlopen('http://172.16.0.1:8001/FieldData/GetData') as response:
+        source = response.read()
 
-data = json.loads(source.decode())
+    data = json.loads(source.decode())
 
-print(json.dumps(data, indent=2))
+    print(json.dumps(data, indent=2))
+    
+    i += 1
+
+    time.sleep(1)
