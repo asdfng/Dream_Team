b2052bb8ed1a2906cffecd19dd23a93ef5f74499
Rice <rice.rodriguez@ttu.edu>
2018-09-18 16:32:20 -0500

    Major updates

--- src/romi_soccer/setup.py
+++ src/romi_soccer/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['romi_soccer'],
     # scripts=['bin/myscript'],
-    package_dir={'': 'src'}
+    package_dir={'': 'include'}
 )
 
 setup(**d)
