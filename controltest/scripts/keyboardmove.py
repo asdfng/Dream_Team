0ae2dfb7390e00376414d21927f2e31020ea4fbe
GregoryGregg <GregoryGregg@users.noreply.github.com>
2018-09-24 17:52:10 -0500

    Final Test?

--- controltest/scripts/keyboardmove.py
+++ controltest/scripts/keyboardmove.py
@@ -7,13 +7,13 @@ a_star = AStar()
 
 while True:
 
-    a_star.motors([1,200,200])
+    a_star.motors([200],[200])
     time.sleep(5)
-    a_star.motors([-200,-200])
+    a_star.motors([-200],[-200])
     time.sleep(5)
-    a_star.motors([-200,200])
+    a_star.motors([-200],[200])
     time.sleep(5)
-    a_star.motors([200,-200])
+    a_star.motors([200],[-200])
     time.sleep(5)
-    a_star.motors([0,0])
+    a_star.motors([0],[0])
     time.sleep(5)
