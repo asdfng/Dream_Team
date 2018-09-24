2ee1ff5fd19158dbfb905c745a6c471bc4a13656
GregoryGregg <GregoryGregg@users.noreply.github.com>
2018-09-24 18:13:19 -0500

    updated dirrec to use python 3

--- controltest/scripts/dirrec.py
+++ controltest/scripts/dirrec.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 sys.path.insert(0, '/home/pi/pololu-rpi-slave-arduino-library-2.0.0/pi')
 import rospy
@@ -8,13 +8,13 @@ from controltest.msg import RoverDirections
 def callback(directions):
 	a_star = AStar()
 	if directions.forward:
-		a_star.motors([int(200),int(200)])
+		a_star.motors(200,200)
 	elif directions.back:
-		a_star.motors([int(-200),int(-200)])
+		a_star.motors(-200,-200)
 	elif directions.left:
-		a_star.motors([int(200),int(-200)])
+		a_star.motors(200,-200)
 	elif directions.right:
-		a_star.motors([int(-200),int(200)])
+		a_star.motors(-200,200)
 
 def main():
 	rospy.init_node('Receiver')
