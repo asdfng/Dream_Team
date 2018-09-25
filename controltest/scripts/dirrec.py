e1f941bda3e539b4e6f5379b39863fb6b137c6a4
GregoryGregg <nicholas.gregg@ttu.edu>
2018-09-24 21:42:15 -0500

    Added a final else statement

--- controltest/scripts/dirrec.py
+++ controltest/scripts/dirrec.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #import sys
 #sys.path.insert(0, '/home/pi/pololu-rpi-slave-arduino-library-2.0.0/pi')
 import rospy
 from a_star import AStar
 from controltest.msg import RoverDirections
 
+#Testing to see what direction is set
 def callback(directions):
 	a_star = AStar()
 	if directions.forward:
@@ -15,6 +16,8 @@ def callback(directions):
 		a_star.motors(200,-200)
 	elif directions.right:
 		a_star.motors(-200,200)
+	else:
+		a_star.motors(0,0)
 
 def main():
 	rospy.init_node('Receiver')
