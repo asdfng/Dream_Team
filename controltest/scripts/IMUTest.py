ed6fd9399e7eb62e82c92d33d8dfeb22883b7e52
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-09-28 23:35:24 -0500

    Added convertions to change the raw values into usable data

--- controltest/scripts/IMUTest.py
+++ controltest/scripts/IMUTest.py
@@ -9,20 +9,26 @@ from lms6 import LSM6
 imu = LSM6()
 imu.enable()
 
+accelSensitivity = 0.061
+accelRatio = 1/1000 #Converting from milligrams to grams
+gyroSensitivity = 0.00875
+
 while True:
     
     #read the IMU, print and wait
     imu.read()
 
+    #Create a dictionary of converted values, use sensitivety data from the spec sheet
     imudata = {
-        "gx": imu.g.x,
-        "gy": imu.g.y,
-        "gz": imu.g.z,
-        "ax": imu.a.x,
-        "ay": imu.a.y,
-        "az": imu.a.z
+        "gx": imu.g.x*gyroSensitivity,
+        "gy": imu.g.y*gyroSensitivity,
+        "gz": imu.g.z*gyroSensitivity,
+        "ax": imu.a.x*accelSensitivity*accelRatio,  
+        "ay": imu.a.y*accelSensitivity*accelRatio,
+        "az": imu.a.z*accelSensitivity*accelRatio
     }
 
+    #Gyro is in degrees per second while accel is in g
     print("G - x: %(gx)s y:%(gy)s z:%(gz)s \nA - x:%(ax)s y:%(ay)s z:%(az)s" % imudata)
     time.sleep(1)
 
