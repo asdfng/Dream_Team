6e96c12c994d7fcc7522cd9a97c9a2f55c066ce9
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-09-30 12:41:02 -0500

    Added quick test code of the encoders of the motors

---
+++ controltest/scripts/EncoderTest.py
@@ -0,0 +1,14 @@
+#Test code for reading data from the Encoders
+#Written by Nicholas Gregg
+
+import time 
+import os
+from a_star import AStar
+
+a_star = AStar()
+
+while True:
+    encoders = a_star.read_encoders()
+    print('%s %s',encoders)
+    time.sleep(.1)
+    os.system('clear')
