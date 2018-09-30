8a86d5ab0ea4a0d7beee4cca7b38c970b0488f15
Rice <rice.rodriguez@ttu.edu>
2018-09-30 17:00:40 -0500

    Pushing before I trash this crap

--- src/romi_soccer/src/romi_soccer/tf_world_map.cpp
+++ src/romi_soccer/src/romi_soccer/tf_world_map.cpp
@@ -4,45 +4,40 @@
 // #include <std_msgs/Float64.h>
 #include <romi_soccer/Homography.h>
 #include <romi_soccer/Map.h>
-// #include <tf2/LinearMath/Quaternion.h>
-class WorldToMap {
-public:
-     // std_msgs::Float64 u;
-     // std_msgs::Float64 v;
-     bool grabbed;
-     double u,v;
-     double tranx_x, trans_y;
-     double center_u, center_v;
-     double center_x, center_y;
-     WorldToMap() {
-          this->grabbed = 0;
-          this->center_x = 0;
-          this->center_y = 0;
-     }
-     void pixCallback(const romi_soccer::Map &msg) {
-          this->center_u = (msg.TopL.x + msg.TopR.x + msg.BotL.x + msg.BotR.x);
-          this->center_v = (msg.TopL.y + msg.TopR.y + msg.BotL.y + msg.BotR.y);
-          this->grabbed = 1;
-     }
-     void callback(const romi_soccer::Homography &msg) {
-          if (grabbed) {
-               static tf2_ros::TransformBroadcaster br;
-               tf2::Quaternion quat(0,0,0,1);
-               this->trans_x = ((msg.q11*center_u+msg.q12*center_v+msg.q13)/(msg.q31*center_u+msg.q32*center_v+msg.q33));
-               this->trans_y = ((msg.q21*center_u+msg.q22*center_v+msg.q23)/(msg.q31*center_u+msg.q32*center_v+msg.q33));
-               tf2::Vector3 translation(trans_x, trans_y, 0);
-               tf2::Transform opt_prime(quat, translation);
-               geometry_msgs::TransformStamped transformStamped;
-               transformStamped.header.stamp = ros::Time::now();
-               transformStamped.header.frame_id = "world";
-               transformStamped.child_frame_id = "map";
-               transformStamped.transform = tf2::toMsg(opt_prime);
-               br.sendTransform(transformStamped);
-          } else {
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/aruco.hpp>
+#include <vector>
+void pixCallback(const romi_soccer::Map &msg) {
+     std::vector<cv::Mat> rotation, translation, normals;
+     std::vector<cv::Point2f> corners_img, corners_tab;
+     cv::Point2f corner_TopL(msg.TopL.x,msg.TopL.y);
+     corners_img.push_back(corner_TopL);
+     cv::Point2f p0(0,0);
+     corners_tab.push_back(p0);
+     cv::Point2f corner_TopR(msg.TopR.x,msg.TopR.y);
+     corners_img.push_back(corner_TopR);
+     cv::Point2f p1(0,8);
+     corners_tab.push_back(p1);
+     cv::Point2f corner_BotL(msg.BotL.x,msg.BotL.y);
+     corners_img.push_back(corner_BotL);
+     cv::Point2f p2(4,0);
+     corners_tab.push_back(p2);
+     cv::Point2f corner_BotR(msg.BotR.x,msg.BotR.y);
+     corners_img.push_back(corner_BotR);
+     cv::Point2f p3(4,8);
+     corners_tab.push_back(p3);
 
-          }
+     cv::Mat homography = cv::findHomography(corners_img,corners_tab);
+     if (H) {
+          ROS_INFO('It worked! H was found.');
      }
-};
+     int solutions = cv::decomposeHomographyMat(homography,cameraMat,rotation,translation,normals);
+     
+
+}
 
 int main(int argc, char **argv) {
      ros::init(argc, argv, 'br_world_to_map');
