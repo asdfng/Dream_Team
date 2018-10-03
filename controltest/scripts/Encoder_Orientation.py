55b1bd65c76a38c40e4391b113b6c31acdc841a2
npratt8 <40443831+npratt8@users.noreply.github.com>
2018-10-02 20:33:16 -0500

    test

--- controltest/scripts/Encoder_Orientation.py
+++ controltest/scripts/Encoder_Orientation.py
@@ -14,7 +14,8 @@ def displacement(right_encoder,left_encoder): #velocity: ft/s, position:
     pi = math.pi
     dist_between_wheels = 0.541339
     right_wheel_rotations = right_encoder/1440                  #converts encoder counts to rotations
-    left_wheel_rotations = left_encoder/1440                    
+    left_wheel_rotations = left_encoder/1440
+    print("rightrotations = %s degrees" % right_wheel_rotations)                   
     right_displacement = right_wheel_rotations*2*pi*.114829     #calculates displacement of right, left and center wheels
     left_displacement = left_wheel_rotations*2*pi*.114829
     center_displacement = (right_displacement + left_displacement)/2
