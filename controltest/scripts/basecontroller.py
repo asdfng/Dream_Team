068ec718060aff69a435319e8ab1d54f4fa374fc
GregoryGregg <nicholas.gregg@ttu.edu>
2018-10-03 20:57:12 -0500

    added method to convert twist message to motor commands

---
+++ controltest/scripts/basecontroller.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+#Code convert geometry/twist msgs to motor commands
+#By: Nicholas Gregg
+
+import rospy
+from a_star import AStar
+from geometry_msgs.msg import Twist
+
+
+mTOft = 3.28084
+a_star = AStar()
+
+#converting the linear and angular message velocities to x and y
+def callback(msg):
+    #Only the angular z-axis and linear x- axis velocity are needed
+    velXL = msg.linear.x * mTOft
+    velZA = msg.angular.z * mTOft
+
+    #Calculated max velocity to be 0.78508 ft/s
+    #fitting -400 to 400 in the range of -0.78508 to 0.78508
+
+    spLinear = int(((velXL)*(.78508 - (-.78508))/(400 - (-400))) - .78508)
+    spAngular = int(((velZA)*(.78508 - (-.78508))/(400 - (-400))) - .78508)
+
+    #the angular data is special it has to be converted to values
+    #for the left and right motors, this will depend on the sign of the value
+
+    spLeft = spLinear + spAngular
+    spRight = spLinear - spAngular
+
+    a_star.motors(spLeft,spRight)
+
+#Setting up the subscriber node
+def listener():
+    rospy.init_node('MotorController', anonymous=True)
+    rospy.subscriber("/cmd_vel", Twist, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
