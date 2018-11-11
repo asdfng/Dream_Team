53490c0c9b7777d393811d942c85aea657412355
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-11-11 13:45:56 -0600

    test

--- src/controltest/scripts/CoordinateTest.py
+++ src/controltest/scripts/CoordinateTest.py
@@ -15,8 +15,8 @@ while True:
     red_square_y = data['Red Team Data']['Square']['Object Center']['Y']
     ball_x = data['Ball']['Object Center']['X']
     ball_y = data['Ball']['Object Center']['Y']
-    mRSX = float(red_square_x - 12)*float(8/(394-12))
-    mRSY = float(red_square_y - 31)*float(8/(221-31))
+    mRSX = float(red_square_x - 12)*float(8/float(394-12))
+    mRSY = float(red_square_y - 31)*float(8/float(221-31))
     mBX = float(ball_x - 12)*float(8)
     mBY = float(ball_y - 31)*float(8)
     print(red_square_x)
