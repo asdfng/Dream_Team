4b8724a6e9035a8eb9d972710ee9cb9a08de5c5a
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-11-17 16:54:22 -0600

    test

--- src/controltest/scripts/Grabber.py
+++ src/controltest/scripts/Grabber.py
@@ -5,12 +5,12 @@ import json, urllib2
 response = urllib2.urlopen('http://192.168.137.1:8001/FieldData/GetData')
 source = response.read()
 data = json.loads(source.decode())
-locations = {bSquare: {'X': data['Blue Team Data']['Square']['Object Center']['X'], 'Y': data['Blue Team Data']['Square']['Object Center']['Y']},
-            bCircle: {'X': data['Blue Team Data']['Circle']['Object Center']['X'], 'Y': data['Blue Team Data']['Circle']['Object Center']['Y']},
-            bTriangle: {'X': data['Blue Team Data']['Triangle']['Object Center']['X'], 'Y': data['Blue Team Data']['Triangle']['Triangle']['Object Center']['Y']},
-            rSquare: {'X': data['Red Team Data']['Square']['Object Center']['X'], 'Y': data['Red Team Data']['Square']['Object Center']['Y']},
-            rCircle: {'X': data['Red Team Data']['Circle']['Object Center']['X'], 'Y': data['Red Team Data']['Circle']['Object Center']['Y']},
-            rTriangle: {'X': data['Red Team Data']['Triangle']['Object Center']['X'], 'Y': data['Red Team Data']['Triangle']['Triangle']['Object Center']['Y']},
-            ball: {'X': data['Ball']['Object Center']['X'], 'Y': data['Ball']['Object Center']['Y']}}
+locations = {'bSquare': {'X': data['Blue Team Data']['Square']['Object Center']['X'], 'Y': data['Blue Team Data']['Square']['Object Center']['Y']},
+            'bCircle': {'X': data['Blue Team Data']['Circle']['Object Center']['X'], 'Y': data['Blue Team Data']['Circle']['Object Center']['Y']},
+            'bTriangle': {'X': data['Blue Team Data']['Triangle']['Object Center']['X'], 'Y': data['Blue Team Data']['Triangle']['Triangle']['Object Center']['Y']},
+            'rSquare': {'X': data['Red Team Data']['Square']['Object Center']['X'], 'Y': data['Red Team Data']['Square']['Object Center']['Y']},
+            'rCircle': {'X': data['Red Team Data']['Circle']['Object Center']['X'], 'Y': data['Red Team Data']['Circle']['Object Center']['Y']},
+            'rTriangle': {'X': data['Red Team Data']['Triangle']['Object Center']['X'], 'Y': data['Red Team Data']['Triangle']['Triangle']['Object Center']['Y']},
+            'ball': {'X': data['Ball']['Object Center']['X'], 'Y': data['Ball']['Object Center']['Y']}}
 
 print(locations)
