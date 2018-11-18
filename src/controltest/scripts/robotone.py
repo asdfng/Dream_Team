2e0eb560fd55e2d204a9537d8b2dcd306deb45d5
npratt8 <40443831+npratt8@users.noreply.github.com>
2018-11-18 16:20:46 -0600

    go

---
+++ src/controltest/scripts/robotone.py
@@ -0,0 +1,16 @@
+import time, json, urllib2
+import timeit 
+import os
+import math
+from a_star import AStar
+from Orientation import straight, grabber, displacement, point_orientation, run, orient, talker
+a_star = AStar()
+
+if __name__ == '__main__':
+    try:
+    last_orientation = talker('rTriangle','ball',0.0)
+    end_orientation = talker('rTriangle','rCircle',last_orientation)
+    except KeyboardInterrupt:
+        a_star.motors(0,0)
+
+print(__name__)
