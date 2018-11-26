d15e7cf0fb303dbb208c12813774b6e10df42889
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-11-25 20:17:07 -0600

    test

--- src/controltest/scripts/robotone.py
+++ src/controltest/scripts/robotone.py
@@ -12,10 +12,8 @@ a_star = AStar() #Not sure that we need this line because it is defined in the o
 if __name__ == '__main__':
     try:
         last_orientation = talker('rTriangle','ball',0.0)
-        i = 0
-        while i < 10:
-            a_star.motors(-50,-50)
-            i = i + 10
+        a_star.motors(-50,-50)
+        time.sleep(1)
         a_star.motors(0,0)
         end_orientation = talker('rTriangle','rCircle',last_orientation)
     except KeyboardInterrupt:
