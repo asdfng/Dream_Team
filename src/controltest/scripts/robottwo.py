ecc6445ccac61e281865eb8651aa5d405b4a56fc
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-11-29 07:02:59 -0600

    test

--- src/controltest/scripts/robottwo.py
+++ src/controltest/scripts/robottwo.py
@@ -8,11 +8,11 @@ from Orientation import grabber, displacement, point_orientation, run, orient, t
 a_star = AStar()
 
 def execute():
-    last_orientation = talker('rSquare','rTriangle',0.0)
+    last_orientation = talker('rCircle','rTriangle',0.0)
     distance = check('rTriangle','bCircle')
     while (distance > 50):
         distance = check('rTriangle','bCircle')
-    end_orientation = talker('rSquare','ball',last_orientation)
+    end_orientation = talker('rCircle','ball',last_orientation)
     a_star.motors(50,50)
     time.sleep(1)
     a_star.motors(0,0)
@@ -25,7 +25,7 @@ def execute():
    
     locations = grabber()
 
-    orientation_input, mag = point_orientation(locations['rSquare']['X'],locations['rSquare']['Y'],339,128)
+    orientation_input, mag = point_orientation(locations['rCircle']['X'],locations['rCircle']['Y'],339,128)
 
     compensated_orientation = (orientation_input - angle_error_offset) % 360
     orient(oldleft_encoder,oldright_encoder,compensated_orientation, end_orientation)
