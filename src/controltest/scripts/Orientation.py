12afbca2efb36856ea9c5184df1e2ebcc6f12632
Nicholas Gregg <GregoryGregg@users.noreply.github.com>
2018-11-29 02:14:16 -0600

    adjusted the thresholds

--- src/controltest/scripts/Orientation.py
+++ src/controltest/scripts/Orientation.py
@@ -159,9 +159,9 @@ def run(me, goal, oLEncoder, oREncoder, compensated_orientation, previous_orient
     previousMeY = locations[me]['Y']
 
     if (goal == 'ball'):
-        mark = 40
+        mark = 30
     else:
-        mark = 50
+        mark = 40
 
     while True:
         
